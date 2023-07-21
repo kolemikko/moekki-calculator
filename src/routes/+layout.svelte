34cd981a29e6c8162818c1fe6dc747dc902b5905
Mikko Kolehmainen <mikko.kolehmainen@metacoregames.com>
2023-07-21 11:28:40 +0300

    Add static-adapter and deployment workflow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
-<script lang='ts'>
+<script lang="ts">
+	export const prerender = true;
 	// The ordering of these imports is critical to your app working properly
 	import '@skeletonlabs/skeleton/themes/theme-rocket.css';
 	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
